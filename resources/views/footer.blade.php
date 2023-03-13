7b38cc72bec0a1ce89f29c7db9c2cd8116d1ceb4
= <=>
2023-03-13 17:28:52 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer style="background-color: black;" class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Farhan Dwi Pramana</b> 2141720125
+    </div>
+    <strong>Copyright &copy; 2023 </strong> All rights reserved.
+</footer>
